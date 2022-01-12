6e183b4fbce301dd4c107482fe3e0ace28592f4b
Dean <dean@deansantillan.com>
2022-01-12 21:29:21 +1000

    task(): added scripts to package json to automate running two projects with one command.

--- server/src/app.js
+++ server/src/app.js
@@ -1,21 +1,31 @@
+// BUILT-IN LIBRARIES
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
-//
+// LOCAL IMPORTS
 import planetsRouter from './routes/planets/planets.router.js';
 
-// init express
+// initialize express
 const app = express();
 
-// middleware
+// MIDDLEWARE //////
+// this is always a must if requesting data from another server.
 app.use(
 	cors({
 		origin: 'http://localhost:3000',
 	})
 );
+// tell the app that you want to process everything in a json format.
 app.use(express.json());
+// serving react app public build statically within the server project
+app.use(express.static(path.join(path.resolve('public'))));
 
-// endpoints
+// ENDPOINTS /////
 app.use(planetsRouter);
+// give the client the index.html without having to type it in manually
+app.get('/', (req, res) => {
+	res.sendFile(path.joing(path.resolve('public')), 'index.html');
+});
 
 export default app;
