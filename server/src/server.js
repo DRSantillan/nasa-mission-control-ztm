b6b97eb94569bf61cec9cb30972777f5dd50e291
Dean <dean@deansantillan.com>
2022-01-15 09:05:47 +1000

    install(mongoose): added mongoose package and setup db cluster on atlas.

--- server/src/server.js
+++ server/src/server.js
@@ -8,6 +8,10 @@ import { loadPlanetsData } from './models/planets.model.js';
 
 // Port for API
 const PORT = process.env.PORT || 8000;
+
+// MONGO_DB Connection
+const MONGO_DB_URL =
+	'mongodb+srv://nasa-api:5savVYyE98wGfsP@nasacluster.cbrcj.mongodb.net/nasa-db?retryWrites=true&w=majority';
 // Create server
 const server = http.createServer(app);
 
